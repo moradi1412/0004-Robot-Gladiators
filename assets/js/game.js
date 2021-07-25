9f56a0da042dca982cc6809abd63959bfe5eff3d
Moradi1412 <moradi.javid@gmail.com>
2021-07-25 14:44:07 -0700

    player prompt

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,15 @@
-window.alert("This is an alert! JavaScript is running!");
+var name = "your name";
+console.log(name);
+
+
+// var playerName = window.prompt ("What is your robot's name"); 
+// window.alert(playerName);
+// console.log (playerName); 
+
+
+
+// function fight (){
+//     window.alert("The fight has begun"); 
+// }
+
+// // fight(); 
