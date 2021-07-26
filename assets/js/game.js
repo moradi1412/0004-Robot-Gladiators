2cfcb8d3b443bb309b368859181d52bc23d8d5a4
Moradi1412 <moradi.javid@gmail.com>
2021-07-26 00:20:35 -0700

    initial game setup

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,15 +1,57 @@
-var name = "your name";
-console.log(name);
+//variable declaration
+var playerName = window.prompt ("What is your robot's name"); 
+var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+var playerHealth = 100; 
+var playerAttack = 10; 
+var playerMoney = 10;
+promptFight = promptFight.toUpperCase(); 
+console.log (promptFight);
+console.log (playerName, playerHealth, playerAttack); 
 
+var enemyName = "Roborto"; 
+var enemyHealth = 50;
+var enemyAttack = 12; 
 
-// var playerName = window.prompt ("What is your robot's name"); 
 // window.alert(playerName);
-// console.log (playerName); 
+
+// create function
+var fight = function() {
+    if (promptFight === "FIGHT") {
+        // remove enemy's health by subtracting the amount set in the playerAttack variable
+        enemyHealth = enemyHealth - playerAttack;
+        console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+      
+        // check enemy's health
+        if (enemyHealth <= 0) {
+          window.alert(enemyName + " has died!");
+        } else {
+          window.alert(enemyName + " still has " + enemyHealth + " health left.");
+        }
+      
+        // remove player's health by subtracting the amount set in the enemyAttack variable
+        playerHealth = playerHealth - enemyAttack;
+        console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+      
+        // check player's health
+        if (playerHealth <= 0) {
+          window.alert(playerName + " has died!");
+        } else {
+          window.alert(playerName + " still has " + playerHealth + " health left.");
+        }
+        // if player choses to skip
+      } else if (promptFight === "skip" || promptFight === "SKIP") {
+        window.alert(playerName + " has chosen to skip the fight!");
+      } else {
+        window.alert("You need to choose a valid option. Try again!");
+      }
+    }
+
+fight();
 
 
 
-// function fight (){
-//     window.alert("The fight has begun"); 
-// }
 
-// // fight(); 
+
+
+  
+
